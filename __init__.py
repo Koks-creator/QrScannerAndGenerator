4f32771f5ba146793d20e95f85e0e7df886fcf95
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2023-08-20 12:56:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 import cv2
 
 app = Flask(__name__)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=20)
 app.secret_key = "guhijih454551"
 QR_SIZE_RANGE = (1, 50)
 QR_BORDER_RANGE = (1, 10)
